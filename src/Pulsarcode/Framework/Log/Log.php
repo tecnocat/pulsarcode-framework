9b04422685ae690bcf6b30fecd20932a52ba0eea
Alpha Nieves <alpha@pulsarcode.com>
2014-11-03 13:31:55 +0100

    Primero Set, despues Get

--- src/Pulsarcode/Framework/Log/Log.php
+++ src/Pulsarcode/Framework/Log/Log.php
@@ -68,36 +68,13 @@ class Log extends Core
     }
 
     /**
-     * Devuelve el registro para ser procesaro
-     *
-     * @return string
-     */
-    public function getMessages()
-    {
-        return $this->messages;
-    }
-
-    /**
-     * Registra todas las líneas y las borra
-     *
-     * @param array $output Salida de una ejecución exec()
-     */
-    public function parseOutput(array $output = array())
-    {
-        foreach ($output as $message)
-        {
-            $this->setMessage($message);
-        }
-    }
-
-    /**
      * Guarda los mensajes en el registro para ser procesados
      *
      * @param string $message      El mensaje a registrar
      * @param array  $placeholders Tokens para remplazar en $message
      * @param bool   $timestamp    Agregar timestamp si es true
      */
-    private function setMessage($message = '', array $placeholders = array(), $timestamp = false)
+    public function setMessage($message = '', array $placeholders = array(), $timestamp = false)
     {
         if (!empty($placeholders))
         {
@@ -121,5 +98,28 @@ class Log extends Core
         }
 
         $this->messages .= $message;
+    }
+
+    /**
+     * Devuelve el registro para ser procesaro
+     *
+     * @return string
+     */
+    public function getMessages()
+    {
+        return $this->messages;
+    }
+
+    /**
+     * Registra todas las líneas y las borra
+     *
+     * @param array $output Salida de una ejecución exec()
+     */
+    public function parseOutput(array $output = array())
+    {
+        foreach ($output as $message)
+        {
+            $this->setMessage($message);
+        }
     }
 }
