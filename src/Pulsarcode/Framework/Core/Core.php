7c67f20668fa1ef5d04c08e404e82e2024100d70
Alpha Nieves <alpha@pulsarcode.com>
2014-11-03 13:32:33 +0100

    Implementamos función para ejecutar comandos en el Core

--- src/Pulsarcode/Framework/Core/Core.php
+++ src/Pulsarcode/Framework/Core/Core.php
@@ -27,4 +27,38 @@ class Core
          */
         Cache::setupCacheObjects();
     }
+
+    /**
+     * Run command and parse output from STOUT + STERR
+     *
+     * @param string $command Command to be run
+     * @param array  $output  Command output
+     * @param bool   $silent  Command run silently?
+     *
+     * @return bool true if exit code of program was 0, false otherwise
+     */
+    protected static function run($command, array &$output = null, $silent = true)
+    {
+        $replace   = array('$' => '\$');
+        $command   = str_replace(array_keys($replace), array_values($replace), $command);
+        $errorFile = tempnam(sys_get_temp_dir(), uniqid('CommandErrors', true));
+        exec($command . ' 2> ' . $errorFile, $output, $exitCode);
+        $errors = file($errorFile, FILE_IGNORE_NEW_LINES);
+        unlink($errorFile);
+
+        if ($silent === false)
+        {
+            if (empty($output) === false)
+            {
+                print_r($output);
+            }
+
+            if (empty($errors) === false)
+            {
+                print_r($errors);
+            }
+        }
+
+        return ($exitCode === 0);
+    }
 }
